09f921a96a91556930938a0ff7f1c1f46f26f81d
aljon-dev <140811659+aljon-dev@users.noreply.github.com>
2024-03-12 00:28:31 +0800

    added

--- app/src/main/java/com/example/e_palengke_vendor/HomeAdmin.java
+++ app/src/main/java/com/example/e_palengke_vendor/HomeAdmin.java
@@ -4,11 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class HomeAdmin extends AppCompatActivity {
+
+    FirebaseAuth auth;
+
+    FirebaseDatabase database;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_admin);
+
+        //Firebase database
+
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+
+
+
+
     }
 }

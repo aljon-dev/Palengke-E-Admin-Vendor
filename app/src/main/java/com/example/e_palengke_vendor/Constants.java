e2b7b2c9816935b962fdcca5e23ee150933cfd9e
michael John <michaeljohn.mabanta@bulsu.edu.ph>
2024-03-14 08:34:53 +0800

    eto may add product na

---
+++ app/src/main/java/com/example/e_palengke_vendor/Constants.java
@@ -0,0 +1,17 @@
+package com.example.e_palengke_vendor;
+
+
+public class Constants {
+
+    //Product Categories
+    public static String[] productCategories = {
+      "Fish",
+      "Vegetables",
+      "Pork",
+      "Beef",
+      "Chicken",
+      "Eggs",
+      "Condiments",
+            "Others"
+    };
+}

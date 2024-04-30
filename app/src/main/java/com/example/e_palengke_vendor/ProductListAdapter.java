5dd70f836368ec76d0804998ed8369d637c2d192
Meow_Dev <aljonjimenez091200@gmail.com>
2024-05-01 04:42:01 +0800

    update

--- app/src/main/java/com/example/e_palengke_vendor/ProductListAdapter.java
+++ app/src/main/java/com/example/e_palengke_vendor/ProductListAdapter.java
@@ -1,18 +1,28 @@
 package com.example.e_palengke_vendor;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class ProductListAdapter extends RecyclerView.Adapter<ProductListAdapter.ItemHolder> {
+
+    private Context context;
+    private ArrayList<ProductModel> ProductList;
 
 
     @NonNull
     @Override
     public ProductListAdapter.ItemHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.productlist,parent,false);
+        return new ItemHolder(view);
     }
 
     @Override
@@ -22,10 +32,14 @@ public class ProductListAdapter extends RecyclerView.Adapter<ProductListAdapter.
 
     @Override
     public int getItemCount() {
-        return 0;
+        return ProductList.size();
     }
 
     public static class ItemHolder extends RecyclerView.ViewHolder {
+
+        TextView productname,price,description,Quantity;
+        ImageView productimg;
+
         public ItemHolder(@NonNull View itemView) {
             super(itemView);
         }
